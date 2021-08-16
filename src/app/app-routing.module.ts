003e32872c7e59fbe78e32458ca2621f03f9e997
Mars <mars@Marss-MacBook.fios-router.home>
2021-08-16 00:01:06 -0400

    fix error so that aot compilation can succeed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+export function getModule(m) { return m.UsersModule; }
+export function loadChildrenFunction () { return import('./users/users.module').then(getModule); } 
+// export function loadChildrenFunction () => import('./users/users.module').then(m => m.UsersModule)
 
-const routes: Routes = [{ path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) }];
+const routes: Routes = [
+  {
+    path: 'users',
+    loadChildren: loadChildrenFunction
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
